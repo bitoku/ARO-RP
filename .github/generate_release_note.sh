04e41015023ddee3bc183feaa6286a13ef4be3b4
Ayato Tokubi <atokubi@redhat.com>
2023-04-26 18:54:06 +0100

    add release-note.yml

---
+++ .github/generate_release_note.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+[ "$(git describe)" != "$(git describe --tags)" ] && echo "Tag is not annotated." && exit 1
+
+CHANGELOG=$1
+CURRENT_TAG=$(git describe --abbrev=0)
+PREVIOUS_TAG=$(git describe --abbrev=0 "$CURRENT_TAG"^)
+
+cat <<EOF > "$CHANGELOG"
+$(git tag -l --format='%(contents)' "$CURRENT_TAG")
+
+<details><summary><b>Changes</b></summary>
+
+$(git log --oneline --no-decorate "$PREVIOUS_TAG".."$CURRENT_TAG")
+
+**Full Changelog**: https://github.com/Azure/ARO-RP/compare/$PREVIOUS_TAG...$CURRENT_TAG
+</details>
+EOF
